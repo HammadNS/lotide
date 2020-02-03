e4aa8e390a75aab6cef45b7f4418a19e20793e70
hammadNS96 <hammad96@outlook.com>
2020-02-03 20:01:26 +0000

    assertEqualFunction

---
+++ assertEqual.js
@@ -0,0 +1,22 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  //let angryface = 🔴;
+
+  if (actual === expected) {
+    console.log("🟢🟢🟢🟢🟢" + " Assertion passed:  " + actual + " === " + expected);
+    return;
+  }
+    
+  if (actual !== expected) {
+    let returnOne = console.log("🔴🔴🔴🔴🔴" + " Assertion failed: " + actual + " !== " + expected);
+
+    return returnOne;
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(2, 1);
